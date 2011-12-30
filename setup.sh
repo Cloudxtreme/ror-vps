4604a6402964bdda8dbc87495baded83d35571ea
Messias Henrique <messiashenrique@gmail.com>
2011-12-30 13:40:07 -0200

    instaling module up_progress

--- setup.sh
+++ setup.sh
@@ -15,13 +15,12 @@ blue="\033[01;34m"
 red="\033[01;31m"
 end_color="\033[00m"
 
-
 #functions
 
 function system_update {
 	apt-get update
 	apt-get -y install aptitude
-	aptitude -y install build-essential zlib1g-dev libssl-dev libreadline5-dev openssh-server libyaml-dev libcurl4-openssl-dev libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev
+	aptitude -y install build-essential zlib1g-dev libssl-dev libreadline5-dev openssh-server libyaml-dev libcurl4-openssl-dev libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev libsqlite3
 
 }
 
@@ -43,6 +42,7 @@ function passenger_install {
 }
 
 function nginx_download {
+  rm -Rf /tmp/$NGINX_VERSION
   cd /tmp
   wget http://nginx.org/download/$NGINX_VERSION.tar.gz
   tar xzf $NGINX_VERSION.tar.gz
@@ -50,26 +50,36 @@ function nginx_download {
 }
 
 function module_up_download {
+  rm -Rf /tmp/up
   cd /tmp
   git clone git://github.com/masterzen/nginx-upload-progress-module.git
   mv nginx-upload-progress-module/ up/
 }
 
-
 function nginx_install_with_passenger_module {
-  rvmsudo passenger-install-nginx-module --auto --nginx-source-dir=/tmp/$NGINX_VERSION --prefix=/opt/nignx --extra-configure-flags='--add-module=/tmp/up'
+  rvmsudo passenger-install-nginx-module --auto --nginx-source-dir=/tmp/$NGINX_VERSION --prefix=/opt/nginx --extra-configure-flags="--add-module=/tmp/up"
 }
 
 #Running
-
-#Updating system
 echo "-------------------------------------------------------------------------------"
-echo -e $green "Iniciando..." $end_color
+echo -e $green "Starting..." $end_color
+echo
+echo -e $green "Updating the system..." $end_color
+echo
+system_update
+echo
+echo -e $green "Downloading o nginx..." $end_color
+echo
+nginx_download
+echo
+echo -e $green "Downloading the module \"upload-progress for nginx\"..." $end_color
 echo
 module_up_download
+echo
+echo -e $green "Downloading and Instaling the passenger with nginx..." $end_color
+echo
 nginx_install_with_passenger_module
 echo
 echo "==============================================================================="
 echo -e $blue "Pronto!" $end_color
 echo "==============================================================================="
-
